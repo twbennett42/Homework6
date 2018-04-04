864835c3ad6e34e05a9e3b633b5b8a460214ae02
twbennett42 <38057897+twbennett42@users.noreply.github.com>
2018-04-03 21:00:41 -0500

    Add files via upload

---
+++ server.R
@@ -0,0 +1,38 @@
+# server.R
+library(shiny)
+source("helpers.R")
+counties <- readRDS("data/counties.rds")
+library(maps)
+library(mapproj)
+
+shinyServer
+(
+  function(input, output){
+    output$map <- renderPlot({
+      
+      data <- switch(input$var, 
+                     "Percent White" = counties$white,
+                     "Percent Black" = counties$black,
+                     "Percent Hispanic" = counties$hispanic,
+                     "Percent Asian" = counties$asian)
+      
+      color <- switch(input$var, 
+                      "Percent White" = "red",
+                      "Percent Black" = "blue",
+                      "Percent Hispanic" = "green",
+                      "Percent Asian" = "darkviolet")
+      
+      legend <- switch(input$var, 
+                       "Percent White" = "Percent White",
+                       "Percent Black" = "Percent Black",
+                       "Percent Hispanic" = "Percent Hispanic",
+                       "Percent Asian" = "Percent Asian")
+      
+      percent_map(var = data, 
+                  color = color, 
+                  legend.title = legend, 
+                  max = input$range[2], 
+                  min = input$range[1])
+    })
+  }
+)
